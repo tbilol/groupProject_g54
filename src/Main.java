835b3fc099312be8260dc868a84b1aacdc94d98c
bilol <tbilol2908@gmail.com>
2025-05-14 22:36:11 +0500

    mainga ish tartibi yozildi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        /*
+        1. Add book -- Bilol
+        2. Show books -- Omonjon
+        3. Remove book -- Bilol
+         */
     }
 }
